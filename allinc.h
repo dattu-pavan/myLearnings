97c9b44ee98e7da0e1a1166bb8268187a114e61b
dattu-pavan <31240172+dattu-pavan@users.noreply.github.com>
2017-08-22 15:38:30 +0530

    Create allinc.h

---
+++ allinc.h
@@ -0,0 +1,7 @@
+/* 
+* This header file contains all the basic required header files for the various C programs and the other 
+* libraries that are generated as part of the project
+*/
+
+#include <conio.h>
+#include <stdio.h>
